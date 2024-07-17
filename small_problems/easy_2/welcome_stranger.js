06933b5187b0ca871049022140be41bdbbaefd06
Sammy Kanan <samkanan@gmail.com>
2024-07-17 10:12:20 -0500

    complete welcome stanger problem

---
+++ small_problems/easy_2/welcome_stranger.js
@@ -0,0 +1,6 @@
+const greetings = (arr, obj) => `Hello, ${arr.join(' ')}! Nice to have a ${obj.title} ${obj.occupation} around.`;
+
+console.log(
+  greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" })
+);
+// logs Hello, John Q Doe! Nice to have a Master Plumber around.
