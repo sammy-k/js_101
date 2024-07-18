6f81c565e6a4715ea371e0c38cc80408e54acc87
Sammy Kanan <samkanan@gmail.com>
2024-07-18 14:31:31 -0500

    update rps to be a best of five game

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -5,31 +5,50 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+let yourScore = 0;
+let computerScore = 0;
+
 while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
+  while (yourScore < 3 && computerScore < 3) {
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+    let choice = readline.question();
 
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt(`That's not a valid choice`);
-    choice = readline.question();
+    while (!VALID_CHOICES.includes(choice)) {
+      prompt(`That's not a valid choice`);
+      choice = readline.question();
+    }
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+    if ((choice === 'rock' && computerChoice === 'scissors') ||
+        (choice === 'paper' && computerChoice === 'rock') ||
+        (choice === 'scissors' && computerChoice === 'paper')) {
+      prompt('You scored!');
+      yourScore += 1;
+    } else if ((choice === 'rock' && computerChoice === 'paper') ||
+    (choice === 'paper' && computerChoice === 'scissors') ||
+    (choice === 'scissors' && computerChoice === 'rock')) {
+      prompt('Computer scored!');
+      computerScore += 1;
+    } else {
+      prompt(`It's a tie`);
+    }
+
+    console.log(`The current score is you: ${yourScore}, computer: ${computerScore} `);
+
+    if (yourScore === 3) {
+      console.log(`You win!`);
+    }
+    if (computerScore === 3) {
+      console.log(`Computer wins!`);
+    }
   }
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
-
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
-    prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-  (choice === 'paper' && computerChoice === 'scissors') ||
-  (choice === 'scissors' && computerChoice === 'rock')) {
-    prompt('Computer wins!');
-  } else {
-    prompt(`It's a tie`);
-  }
+  yourScore = 0;
+  computerScore = 0;
 
   prompt(`Do you want to play again (y/n)?`);
   let answer = readline.question().toLowerCase();
