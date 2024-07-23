5d0eb250270fa43af9ac5095c1503170fffa3cfe
Sammy Kanan <samkanan@gmail.com>
2024-07-22 20:21:50 -0500

    complete question 2

--- lesson_3/practice_problems_medium_1.js
+++ lesson_3/practice_problems_medium_1.js
@@ -1,11 +1,28 @@
 // Question 1
-const flintstonesRock = () => {
-  let message = 'The Flintstones Rock!';
-  let space = ' ';
-  for (let idx = 1; idx <= 10; idx += 1) {
-    console.log(message);
-    message = space + message;
-  }
-};
+// const flintstonesRock = () => {
+//   let message = 'The Flintstones Rock!';
+//   let space = ' ';
+//   for (let idx = 1; idx <= 10; idx += 1) {
+//     console.log(message);
+//     message = space + message;
+//   }
+// };
 
-console.log(flintstonesRock());
+// console.log(flintstonesRock());
+
+// Question 2
+// let munstersDescription = "The Munsters are creepy and spooky.";
+
+// const swapCase = (str) => {
+//   let swapedStr = '';
+//   for (let idx = 0; idx < str.length; idx += 1) {
+//     if (str[idx] === str[idx].toLowerCase(0)) {
+//       swapedStr += str[idx].toUpperCase();
+//     } else {
+//       swapedStr += str[idx].toLowerCase();
+//     }
+//   }
+//   return swapedStr;
+// };
+
+// console.log(swapCase(munstersDescription));
