cccae3f8f34b997008079e3682f208813ef8893d
Sammy Kanan <samkanan@gmail.com>
2024-07-22 19:52:50 -0500

    complete easy 3, question 5

--- lesson_3/practice_problems_easy_3.js
+++ lesson_3/practice_problems_easy_3.js
@@ -1,5 +1,5 @@
 // Question 1
-let numbers = [1, 2, 3, 4];
+// let numbers = [1, 2, 3, 4];
 
 // Method 1
 // while (numbers.length > 0) {
@@ -12,5 +12,19 @@ let numbers = [1, 2, 3, 4];
 // }
 
 // Method 3
-numbers = numbers.slice(numbers.length);
-console.log('numbers array: ', numbers);
+// numbers = numbers.slice(numbers.length);
+// console.log('numbers array: ', numbers);
+
+// Question 5
+
+// Method 1
+// const isColorValid = (color) => color === 'blue' || color === 'green';
+
+// Method 2
+// const isColorValid = (color) => ['blue', 'green'].includes(color);
+
+// console.log(isColorValid('pink')); // false
+// console.log(isColorValid('green')); // true
+// console.log(isColorValid('blue')); // true
+
+
