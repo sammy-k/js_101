1e10158b17f5c5d9d7677fcde5c55f599c9445a5
Sammy Kanan <samkanan@gmail.com>
2024-07-23 09:47:34 -0500

    create file for hard 1 problems

---
+++ lesson_3/practice_problems_hard_1.js
@@ -0,0 +1,23 @@
+// Question 3
+
+function messWithVars(one, two, three) {
+  one = two;
+  two = three;
+  three = one;
+}
+
+let one = ["one"];
+let two = ["two"];
+let three = ["three"];
+
+messWithVars(one, two, three);
+
+console.log(`one is: ${one}`);
+console.log(`two is: ${two}`);
+console.log(`three is: ${three}`);
+
+// Question 4
+
+// variables: boo, scare, myBoo
+// primitives: halloweenCollection keys
+// objects: halloweenCollection, boo
