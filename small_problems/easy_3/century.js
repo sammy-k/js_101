ef042f921545ad8ed1a5bd6d879ac5d75e53df89
Sammy Kanan <samkanan@gmail.com>
2024-07-25 10:51:16 -0500

    complete easy 3 - century problem

--- small_problems/easy_3/century.js
+++ small_problems/easy_3/century.js
@@ -1,18 +1,24 @@
-const century = (year) => { // NOTE: year is a string
-  let centuryNum = Math.ceil(year / 100).toString();
-  // take account of all last numbers 
-  if ()
-  if ([4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].includes(Number(centuryNum))) {
-    return centuryNum += 'th';
+const century = (year) => {
+  let centuryYear = year % 100 === 0 ? year / 100 : Math.ceil(year / 100);
+  let centuryYearString = String(centuryYear);
+  if (centuryYear !== 11 && centuryYearString.slice(centuryYearString.length - 1) === '1') {
+    centuryYearString += 'st';
+  } else if (centuryYear !== 12 && centuryYearString.slice(centuryYearString.length - 1) === '2') {
+    centuryYearString += 'nd';
+  } else if (centuryYear !== 13 && centuryYearString.slice(centuryYearString.length - 1) === '3') {
+    centuryYearString += 'rd';
+  } else {
+    centuryYearString += 'th';
   }
+  return centuryYearString;
 };
 
 console.log(century(2000));        // "20th"
-century(2001);        // "21st"
-century(1965);        // "20th"
-century(256);         // "3rd"
-century(5);           // "1st"
-century(10103);       // "102nd"
-century(1052);        // "11th"
-century(1127);        // "12th"
-century(11201);       // "113th"
+console.log(century(2001));        // "21st"
+console.log(century(1965));        // "20th"
+console.log(century(256));         // "3rd"
+console.log(century(5));           // "1st"
+console.log(century(10103));       // "102nd"
+console.log(century(1052));        // "11th"
+console.log(century(1127));        // "12th"
+console.log(century(11201));       // "113th"
